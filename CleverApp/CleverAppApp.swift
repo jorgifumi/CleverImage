8ace5b33d343ab1ad5f9c381aa01d87e28a757bd
Jorge Lucena <jorgifumi@gmail.com>
2023-03-27 13:40:15 +0200

    Create new SwiftUI target

---
+++ CleverApp/CleverAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleverAppApp.swift
+//  CleverApp
+//
+//  Created by Jorge Lucena on 27/3/23.
+//
+
+import SwiftUI
+
+@main
+struct CleverAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
